3fad5074f3eafa9156fd5a087f1e73b9e0da4a6c
dyyyywenn <67235223+dyyyywenn@users.noreply.github.com>
2020-08-26 18:52:39 +0700

    Add files via upload

--- Challenge Kotlin Fundamental/src/main/kotlin/Challenge.kt
+++ Challenge Kotlin Fundamental/src/main/kotlin/Challenge.kt
@@ -1,55 +1,73 @@
+import java.awt.SystemColor.menu
 import java.util.*
+public var input = Scanner(System.`in`);
+fun main() {
 
-fun main(args: Array<String>) {
-    val input:Scanner = Scanner(System.`in`)
 
     println("===============================")
     print("Nama :")
-    readLine()
-    print("Kelas :")
-    readLine()
-    print("No Absen:")
-    val Absen =input.nextInt()
-    println("================================")
-    println("Kamu ingin menghitung apa?")
-    println("""
-        1. Volume Kerucut
-        2. Volume Tabung
-    """.trimIndent())
-    println("=================================")
-    print("Pilihanmu:")
-    val hitung = input.nextInt()
-    val PI = 3.14
-    val volume : Double
-    if (hitung == 1) {
+    val nama: String? = input.nextLine()
 
-        print("Masukkan tinggi:")
-        val tinggi =input.nextInt()
-        print("Masukkan jari-jari:")
-        val jari = input.nextInt()
-        val kerucut = hitungkerucut(tinggi.toDouble(),PI,jari.toDouble())
-        volume = kerucut.toDouble()
-        println("Volume dari kerucut yang mempunyai tinggi $tinggi, jari-jari $jari,  adalah $volume")
-
-        }else if (hitung == 2){
-        print("Masukkan tinggi:")
-        val tinggi = input.nextInt()
-        print("Masukkan jari:")
-        val jari = input.nextInt()
-        val tabung = hitungTabung(tinggi.toDouble(),PI,jari.toDouble())
-        volume = tabung.toDouble()
-        println("Volume dari tabung yang mempunyai tinggi $tinggi, jari-jari $jari,  adalah $volume")
-        }else{
-        print("Menu tidak ada")
+    if (nama.isNullOrEmpty()) {
+        println("Mohon diisi")
+        main()
     }
+    print("Kelas :")
+    val kelas: String = input.nextLine()
+    print("No Absen:")
+    val Absen = input.nextInt()
+    print("GitHub:")
+    val git: String? = null
+    val gitLength = git?.length ?: "dyyyywenn"
+    println(gitLength)
+    println("================================")
+    menu()
+}
+fun menu(){
+    println("1. Kerucut\n2. Tabung\n ")
+    print ("Pilih Menu Diatas : ")
+    val pill:Int? = input.nextInt()
+    when (pill) {
+        1 -> kerucut()
+        2 -> tabung()
+        else -> {
+            println("Pilihan tidak ada")
+            menu()
+        }
+    }
+}
+fun kerucut(){
+    print("Masukan Tinggi Kerucut : ")
+    val tinggi:Double = input.nextDouble()
+    print("Masukan Jari-jari Kerucut : ")
+    val jari:Double = input.nextDouble()
+    val v = hitungKerucut(tinggi.toDouble(),jari.toDouble())
+    val volume : Double
+    volume = v.toDouble()
+    println("Volume dari balok yang mempunyai panjang $tinggi, lebar $jari, dam tinggi $tinggi ini adalah $volume")
+}
+fun hitungKerucut(t: Double,r: Double):Double{
+    val v = (0.33333) * Math.PI * (r * r) * t
+    return v.toDouble()
+
+}
+fun tabung(){
+    println("===========================");
+    print("Masukan Tinggi tabung : ");
+    var tinggi:Double? = null
+    tinggi = input.nextDouble();
+    print("Masukan jari-jari tabung: ");
+    var jari:Double? = null
+    jari = input.nextDouble();
+    val v = hitungTabung(tinggi.toDouble(),jari.toDouble())
+    val volume : Double
+    volume = v.toDouble()
+    println("Volume dari balok yang mempunyai panjang $tinggi, lebar $jari, dam tinggi $tinggi ini adalah $volume")
 
 
 }
-fun hitungkerucut(t : Double,pi: Double,r :Double):Float {
-    val kerucut = 1/3 * pi * r * r * t
-    return kerucut.toFloat()
-}
-fun hitungTabung(t: Double,pi: Double,r: Double):Float{
-    val tabung =  pi * r * r * t
-    return tabung.toFloat()
+fun hitungTabung(t: Double, r: Double):Double{
+    val v = Math.PI * r * r * t
+    return v.toDouble()
+
 }
